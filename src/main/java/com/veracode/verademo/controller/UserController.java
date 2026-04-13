a04fbc52f6107b71ff0713383558499331c48f56
Stuart Sessions <stsessions@veracode.com>
2026-04-13 09:00:03 -0400

    reverted sql injection fix

--- src/main/java/com/veracode/verademo/controller/UserController.java
+++ src/main/java/com/veracode/verademo/controller/UserController.java
@@ -147,7 +147,7 @@ public class UserController {
 		}
 
 		Connection connect = null;
-		PreparedStatement sqlStatement = null;
+		Statement sqlStatement = null;
 
 		try {
 			// Get the Database Connection
@@ -157,13 +157,12 @@ public class UserController {
 
 			/* START EXAMPLE VULNERABILITY */
 			// Execute the query
-			logger.info("Creating the PreparedStatement");
-			String sqlQuery = "select username, password, password_hint, created_at, last_login, real_name, blab_name from users where username=? and password=?";
-			PreparedStatement preparedStatement = connect.prepareStatement(sqlQuery);
-			preparedStatement.setString(1, username);
-			preparedStatement.setString(2, md5(password));
-			logger.info("Execute the PreparedStatement");
-			ResultSet result = preparedStatement.executeQuery();
+			logger.info("Creating the Statement");
+			String sqlQuery = "select username, password, password_hint, created_at, last_login, real_name, blab_name from users where username='"
+					+ username + "' and password='" + md5(password) + "';";
+			sqlStatement = connect.createStatement();
+			logger.info("Execute the Statement");
+			ResultSet result = sqlStatement.executeQuery(sqlQuery);
 			/* END EXAMPLE VULNERABILITY */
 
 			// Did we find exactly 1 user that matched?
